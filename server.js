d301888fe743f23527246f20852f76105ac80d04
mdnsudharsan-create <mdnsudharsan@gmail.com>
2026-04-20 17:42:02 +0530

    Refactor server.js for improved logging and structure

--- server.js
+++ server.js
@@ -4,26 +4,43 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-
 const io = new Server(server, {
-  cors: { origin: "*" }
+  cors: {
+    origin: "*"
+  }
 });
 
-io.on("connection", (socket) => {
-  console.log("Client connected");
-
-  socket.on("control", (data) => {
-    io.emit("control", data.steer + "," + data.move);
-  });
-
-  socket.on("battery", (val) => {
-    io.emit("battery", val);
-  });
-});
-
-// simple route (so browser shows something)
+// Simple test route
 app.get("/", (req, res) => {
-  res.send("Robot server is running 🚗");
+  res.send("🚗 Robot server is running!");
 });
 
-server.listen(process.env.PORT || 3000);
+// When client connects
+io.on("connection", (socket) => {
+  console.log("🔌 Client connected");
+
+  // Receive control data from phone
+  socket.on("control", (data) => {
+    console.log("🎮 Control:", data);
+
+    // Send to all (ESP32 or other clients)
+    io.emit("control", data);
+  });
+
+  // Receive battery from ESP32
+  socket.on("battery", (value) => {
+    console.log("🔋 Battery:", value);
+    io.emit("battery", value);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("❌ Client disconnected");
+  });
+});
+
+// IMPORTANT FOR RENDER
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("🚀 Server running on port " + PORT);
+});
