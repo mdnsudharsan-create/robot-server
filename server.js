43cb5fe1cfad4027ddaad6ea1e16deafc6197ac9
mdnsudharsan-create <mdnsudharsan@gmail.com>
2026-04-20 18:03:20 +0530

    Refactor server response messages and socket events

--- server.js
+++ server.js
@@ -4,41 +4,27 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
+  cors: { origin: "*" }
 });
 
-// Simple test route
 app.get("/", (req, res) => {
-  res.send("Robot server is running!");
+  res.send("🚗 Server running");
 });
 
-// When client connects
 io.on("connection", (socket) => {
   console.log("Client connected");
 
-  // Receive control data from phone
   socket.on("control", (data) => {
-    console.log("Control:", data);
-
-    // Send to all (ESP32 or other clients)
     io.emit("control", data);
   });
 
-  // Receive battery from ESP32
-  socket.on("battery", (value) => {
-    console.log("Battery:", value);
-    io.emit("battery", value);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
+  socket.on("battery", (val) => {
+    io.emit("battery", val);
   });
 });
 
-// IMPORTANT FOR RENDER
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
